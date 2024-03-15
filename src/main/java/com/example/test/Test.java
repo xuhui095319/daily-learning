891194ca135f794e517a3e3fe3c042785597e89b
xy_zhangk <zhangk@staryea.com>
2024-03-15 10:26:26 +0800

    git init

---
+++ src/main/java/com/example/test/Test.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Map map = new HashMap();
+        map.get(null);
+    }
+}
